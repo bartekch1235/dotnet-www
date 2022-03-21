99940b7f5b437e0a7f8199d552b1ef56b385b323
Bartłomiej Chmielewski <gumif@wp.pl>
2022-03-21 17:43:56 +0100

    zadanie 2 skonczone

---
+++ trololo/Models/Class.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace trololo.Models
+{
+    public class FizzBuzz
+    {
+
+        [Required,Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakredu {1} i {2}.")]
+
+
+        [Display(Name = "Twój szczęśliwy numerek")]
+        public int? Number { get; set; }
+        public string CheckRange()
+        {
+            if(Number == null)
+            {
+                
+                return "to nie powinno tu byc";
+            }
+            if(Number%15==0)
+                return "FizzBuzz";
+            if (Number % 5 == 0)
+                return "Buzz";
+            if (Number % 3 == 0)
+                return "Fizz";
+            else
+                return "nieFajnaLiczba";
+        }
+
+    }
+}
--- trololo/Pages/Index.cshtml.cs
+++ trololo/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using trololo.Models;
 
 namespace trololo.Pages
 {
@@ -11,10 +12,36 @@ namespace trololo.Pages
         {
             _logger = logger;
         }
-
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+
+            }
 
         }
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+
+            }
+            if (!ModelState.IsValid)
+            {
+
+                ViewData["a"] = FizzBuzz.CheckRange();
+            }
+            return Page();
+
+        }
+        
+
+
     }
 }
